f600b2f6c310186d477f469d490bffc808d998d7
Yosshii-42 <161409687+Yosshii-42@users.noreply.github.com>
2024-09-14 16:13:04 +0900

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -1,30 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: yotsurud <yotsurud@student.42tokyo.>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/28 15:24:19 by yotsurud          #+#    #+#             */
-/*   Updated: 2024/05/02 18:13:28 by yotsurud         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <sys/types.h>
-# include <sys/stat.h>
-# include <fcntl.h>
-# include <stdio.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE        2
-# endif
-# define FALSE -5
+ #ifndef BUFFER_SIZE
+ # define BUFFER_SIZE 42
+ #endif
 
-char	*get_next_line(int fd);
+#define FAIL -2
 
 #endif
